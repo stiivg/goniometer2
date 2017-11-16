f5340fdf5a6657ada611ed48fd09b555566aac7a
Steven Gallagher <stevengallagher@stevens-mbp.lan>
2017-11-16 11:39:22 -0800

    First commit. Dot draws and is draggable sort of

--- goniometer/ViewController.swift
+++ goniometer/ViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    @IBOutlet weak var kneeImage: UIImageView!
+    @IBOutlet var panGesture: UIPanGestureRecognizer!
+    
+    let dotLayer = CAShapeLayer()
+
+    //MARK Actions
+    @IBAction func handleDotPan(_ gestureRecognizer: UIPanGestureRecognizer) {
+        if gestureRecognizer.state == .began || gestureRecognizer.state == .changed {
+            let translation = gestureRecognizer.translation(in: self.view)
+            dotLayer.position = CGPoint(x: translation.x, y: translation.y)
+           // note: 'view' is optional and need to be unwrapped
+//            sender.view!.center = CGPoint(x: sender.view!.center.x + translation.x, y: sender.view!.center.y + translation.y)
+//            sender.setTranslation(CGPoint.zero, in: self.view)
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let dotPath = UIBezierPath(ovalIn: CGRect(x: 200, y: 300, width: 12, height: 12))
+        
+        dotLayer.path = dotPath.cgPath
+        dotLayer.strokeColor = UIColor.red.cgColor
+        
+        view.layer.addSublayer(dotLayer)
+        
+        view.isUserInteractionEnabled = true
+//        view.addGestureRecognizer(panGesture)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+   
 
 }
 
