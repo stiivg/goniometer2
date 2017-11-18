7987a1dd2dc7798f50e629aa7b2c744788111935
Steven Gallagher <stevengallagher@stevens-mbp.lan>
2017-11-18 11:59:11 -0800

    Changed dot panning to redraw the path on each pan motion which is faster than changing the CAShapeLayer position. Compiled to 10.3 for my iPhone

--- goniometer/ViewController.swift
+++ goniometer/ViewController.swift
@@ -21,9 +21,14 @@ class ViewController: UIViewController {
     let middleDotLayer = CAShapeLayer()
     let endDotLayer = CAShapeLayer()
     
+    let beginLineLayer = CAShapeLayer()
+    let endLineLayer = CAShapeLayer()
+
     var beginDotPosition = CGPoint(x: 100, y: 300)
     var middleDotPosition = CGPoint(x: 100, y: 200)
     var endDotPosition = CGPoint(x: 150, y: 200)
+    var dotPositions = [CGPoint]()
+    var movingDotIndex = 1
     
     var panStart = CGPoint(x: 0, y: 0)
     
@@ -31,85 +36,94 @@ class ViewController: UIViewController {
     @IBAction func handleDotPan(_ gestureRecognizer: UIPanGestureRecognizer) {
         if gestureRecognizer.state == .began {
             panStart = gestureRecognizer.location(in: self.view)
-            let beginDistance = hypot(beginDotLayer.position.x - panStart.x, beginDotLayer.position.y - panStart.y)
-            let middleDistance = hypot(middleDotLayer.position.x - panStart.x, middleDotLayer.position.y - panStart.y)
-            let endDistance = hypot(endDotLayer.position.x - panStart.x, endDotLayer.position.y - panStart.y)
-            //   Set dotLayer to the nearest dot to move
-            if beginDistance < middleDistance {
-                if beginDistance < endDistance {
-                    dotLayer = beginDotLayer
-                } else {
-                    dotLayer = endDotLayer
-                }
-            } else {
-                if middleDistance < endDistance {
-                    dotLayer = middleDotLayer
-                } else {
-                    dotLayer = endDotLayer
-                }
-            }
+            let beginDistance = hypot(dotPositions[0].x - panStart.x, dotPositions[0].y - panStart.y)
+            let middleDistance = hypot(dotPositions[1].x - panStart.x, dotPositions[1].y - panStart.y)
+            let endDistance = hypot(dotPositions[2].x - panStart.x, dotPositions[2].y - panStart.y)
             
-            dotStartPosition = dotLayer.position
+            //   Set movingDotIndex to the nearest dot to move
+            let nearestDot = min(min(beginDistance, middleDistance), endDistance)
+            switch nearestDot {
+            case beginDistance:
+                movingDotIndex = 0
+            case middleDistance:
+                movingDotIndex = 1
+            case endDistance:
+                movingDotIndex = 2
+            default:
+                movingDotIndex = 0
+            }
+
+            dotStartPosition = dotPositions[movingDotIndex]
         } else if gestureRecognizer.state == .changed {
             let translation = gestureRecognizer.translation(in: self.view)
-            dotLayer.position = CGPoint(x: dotStartPosition.x + translation.x, y: dotStartPosition.y + translation.y)
+            dotPositions[movingDotIndex] = CGPoint(x: dotStartPosition.x + translation.x, y: dotStartPosition.y + translation.y)
+            
+            drawDots()
+            drawLines()
         }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        dotPositions = [beginDotPosition, middleDotPosition, endDotPosition]
         
         drawDots()
-        
+        drawLines()
+        view.layer.addSublayer(beginLineLayer)
+        view.layer.addSublayer(endLineLayer)
+
+ 
+        view.layer.addSublayer(beginDotLayer)
+        view.layer.addSublayer(middleDotLayer)
+        view.layer.addSublayer(endDotLayer)
         view.isUserInteractionEnabled = true
         
     }
     
     func drawDots() {
-        let beginDotPath = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: 12, height: 12))
+        let beginOrigin = CGPoint(x: dotPositions[0].x - 6, y: dotPositions[0].y - 6)
+        let beginDotPath = UIBezierPath(ovalIn: CGRect(origin: beginOrigin, size: CGSize(width: 12, height: 12)))
         
         beginDotLayer.path = beginDotPath.cgPath
         beginDotLayer.fillColor = UIColor.red.cgColor
-        beginDotLayer.position = beginDotPosition
 
-        let middleDotPath = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: 12, height: 12))
-        
+        let middleOrigin = CGPoint(x: dotPositions[1].x - 6, y: dotPositions[1].y - 6)
+        let middleDotPath = UIBezierPath(ovalIn: CGRect(origin: middleOrigin, size: CGSize(width: 12, height: 12)))
+
         middleDotLayer.path = middleDotPath.cgPath
         middleDotLayer.fillColor = UIColor.green.cgColor
-        middleDotLayer.position = middleDotPosition
 
-        let endDotPath = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: 12, height: 12))
-        
+        let endOrigin = CGPoint(x: dotPositions[2].x - 6, y: dotPositions[2].y - 6)
+        let endDotPath = UIBezierPath(ovalIn: CGRect(origin: endOrigin, size: CGSize(width: 12, height: 12)))
+
         endDotLayer.path = endDotPath.cgPath
         endDotLayer.fillColor = UIColor.blue.cgColor
-        endDotLayer.position = endDotPosition
-
-        view.layer.addSublayer(beginDotLayer)
-        view.layer.addSublayer(middleDotLayer)
-        view.layer.addSublayer(endDotLayer)
 
     }
     
-//    func createRectangle() {
-//        // Initialize the path.
-//        let path = UIBezierPath()
-//
-//        // Specify the point that the path should start get drawn.
-//        path.move(to: CGPoint(x: 0.0, y: 0.0))
-//
-//        // Create a line between the starting point and the bottom-left side of the view.
-//        path.addLine(to: CGPoint(x: 0.0, y: self.frame.size.height))
-//
-//        // Create the bottom line (bottom-left to bottom-right).
-//        path.addLine(to: CGPoint(x: self.frame.size.width, y: self.frame.size.height))
-//
-//        // Create the vertical line from the bottom-right to the top-right side.
-//        path.addLine(to: CGPoint(x: self.frame.size.width, y: 0.0))
-//
-//        // Close the path. This will create the last line automatically.
-//        path.close()
-//    }
+    func drawLines() {
+        let beginPath = UIBezierPath()
+        beginPath.move(to: dotPositions[0])
+        beginPath.addLine(to: dotPositions[1])
+        
+        beginLineLayer.path = beginPath.cgPath
+        beginLineLayer.lineWidth = 1.0
+        //        beginLineLayer.fillColor = nil
+        //        beginLineLayer.opacity = 1.0
+        beginLineLayer.strokeColor = UIColor.cyan.cgColor
+        
+        let endPath = UIBezierPath()
+        endPath.move(to: dotPositions[1])
+        endPath.addLine(to: dotPositions[2])
+        
+        endLineLayer.path = endPath.cgPath
+        endLineLayer.lineWidth = 2.0
+        //        endLineLayer.fillColor = nil
+        //        endLineLayer.opacity = 1.0
+        endLineLayer.strokeColor = UIColor.black.cgColor
+        
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
