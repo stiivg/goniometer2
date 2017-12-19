d09075fe31a266975afc8a7c6c5fdbfeec479fff
Steven Gallagher <stevengallagher@stevens-mbp.lan>
2017-12-18 17:14:16 -0800

    Improved angle calculation for sign to draw the arc and place the angle text

--- goniometer/AngleTool.swift
+++ goniometer/AngleTool.swift
@@ -34,6 +34,9 @@ class AngleTool {
     let dotLineWidth = CGFloat(2)
     let lineWidth = CGFloat(5)
     let ExtensionLength = CGFloat(60)
+    let angleTextDistance = CGFloat(60)
+    let arcRadius = CGFloat(40)
+    let arcLineWidth = CGFloat(1)
     
     let beginDotLayer = CAShapeLayer()
     let middleDotLayer = CAShapeLayer()
@@ -46,6 +49,7 @@ class AngleTool {
 
     let beginLineLayer = CAShapeLayer()
     let endLineLayer = CAShapeLayer()
+    let arcLineLayer = CAShapeLayer()
     
     var angleLabel = UILabel()
 
@@ -104,6 +108,7 @@ class AngleTool {
         imageView.layer.addSublayer(beginDotLayer)
         imageView.layer.addSublayer(middleDotLayer)
         imageView.layer.addSublayer(endDotLayer)
+        imageView.layer.addSublayer(arcLineLayer)
         
         imageView.layer.addSublayer(textLayer)
         
@@ -156,6 +161,7 @@ class AngleTool {
     func drawTool() {
         drawDots()
         drawLines()
+        drawAngleArc()
         drawAngle()
     }
 
@@ -236,11 +242,12 @@ class AngleTool {
 
     fileprivate func initTextLayer() {
         let fontName: CFString = "HelveticaNeue" as CFString
-        textLayer.frame.size = CGSize(width: 100, height: 50)
+        textLayer.frame.size = CGSize(width: 80, height: 30)
         textLayer.font = CTFontCreateWithName(fontName, 8, nil)
         textLayer.fontSize = 24
         textLayer.borderColor = UIColor.black.cgColor
         textLayer.borderWidth = 2.0
+//        textLayer.isOpaque = true
         textLayer.opacity = 0.4
         //        textLayer.anchorPoint = CGPoint(x: 1, y: 1)
         
@@ -250,26 +257,100 @@ class AngleTool {
         textLayer.contentsScale = UIScreen.main.scale
     }
     
+    fileprivate func drawAngleArc() {
+        let arcLineOrigin = CGPoint(x: dotPositions[1].x, y: dotPositions[1].y)
+        let arcLinePath = UIBezierPath.init(arcCenter: arcLineOrigin, radius: arcRadius, startAngle: minorArmAngle(), endAngle: mainArmAngle() , clockwise: true)
+        
+//        let arcLinePath = UIBezierPath(ovalIn: CGRect(origin: arcLineOrigin, size: CGSize(width: 2 * arcRadius, height: 2 * arcRadius)))
+        
+        arcLineLayer.path = arcLinePath.cgPath
+        arcLineLayer.lineWidth = arcLineWidth
+        arcLineLayer.strokeColor = UIColor.black.cgColor
+        arcLineLayer.fillColor = UIColor.clear.cgColor
+        
+    }
+    
     func drawAngle() {
         calcAngle()
         let angleText = String(format: "%.1f", measuredAngle) + "\u{00B0}"
-        let textOrigin = dotPositions[1]
+        let textCenter = CGPoint(x: 40, y: 15)
+        var textOrigin = textPoint()
+        textOrigin.x -= textCenter.x
+        textOrigin.y -= textCenter.y
+
+
         textLayer.string = angleText
         textLayer.frame.origin = textOrigin
 
       
     }
+    fileprivate func textPoint() -> CGPoint {
+        let textAngle =  mainArmAngle() // + measuredAngle / 2 * CGFloat(CGFloat.pi / 180)
+        let textOffset = CGPoint(x: angleTextDistance * sin(textAngle), y: angleTextDistance * cos(textAngle))
+        let textPoint = CGPoint(x: dotPositions[1].x + textOffset.x, y: dotPositions[1].y + textOffset.y)
+        
+        return textPoint
+    }
+    
+    //use difference of main arm angle and minor arm angle
+    fileprivate func calcAngle() {
+        let diff = (minorArmAngle() - mainArmAngle()) * CGFloat(180 / CGFloat.pi)
+        let absDiff = abs(diff)
+        measuredAngle = diff
+        //Reduce angles over 180
+        if absDiff > 180{
+            measuredAngle = 360 - absDiff
+            if diff > 0 { //change sign if original large angle was positive
+                measuredAngle = -measuredAngle
+            }
+        }
+    }
+    
+    //Down is zero, positive is counter clockwise
+    //down = 0, right = 1.5, left = -1.5, up +/-3
+    fileprivate func mainArmAngle() -> CGFloat {
+        let dX = dotPositions[1].x - dotPositions[0].x
+        let dY = dotPositions[1].y - dotPositions[0].y
+        var mainArmAngle = atan(dX / dY)
+        if dY < 0 {
+            if dX < 0 {
+                mainArmAngle -= CGFloat.pi
+            } else {
+                mainArmAngle += CGFloat.pi
+            }
+        }
+        print("Main= " + String(format: "%.1f", mainArmAngle))
+        return mainArmAngle
+    }
+    
+    //Down is zero, positive is counter clockwise
+    //down = 0, right = 1.5, left = -1.5, up +/-3
+    fileprivate func minorArmAngle() -> CGFloat {
+        let dX = dotPositions[2].x - dotPositions[1].x
+        let dY = dotPositions[2].y - dotPositions[1].y
+        var minorArmAngle = atan(dX / dY)
+        if dY < 0 {
+            if dX < 0 {
+                minorArmAngle -= CGFloat.pi
+            } else {
+                minorArmAngle += CGFloat.pi
+            }
+        }
+        print("Minor= " + String(format: "%.1f", minorArmAngle))
+        return minorArmAngle
+    }
     
 
-    func calcAngle() {
-        let distance01 = hypot(dotPositions[0].x - dotPositions[1].x, dotPositions[0].y - dotPositions[1].y)
-        let distance12 = hypot(dotPositions[1].x - dotPositions[2].x, dotPositions[1].y - dotPositions[2].y)
-        let distance02 = hypot(dotPositions[0].x - dotPositions[2].x, dotPositions[0].y - dotPositions[2].y)
-        
-        let cosAngleNumerator = pow(distance01, 2) +  pow(distance12, 2)  - pow(distance02, 2)
-        let cosAngleDenominator = 2 * distance01 * distance12
-        let cosAngle = cosAngleNumerator / cosAngleDenominator
-        let angleRadians = acos(cosAngle)
-        measuredAngle = 180 - (angleRadians * CGFloat(180 / CGFloat.pi));
-    }
+//    //Using lengths of sides then cosine rule: b^2 = a^2 + c^2 - 2acCosB
+//    func calcAngle() {
+//        let distance01 = hypot(dotPositions[0].x - dotPositions[1].x, dotPositions[0].y - dotPositions[1].y)
+//        let distance12 = hypot(dotPositions[1].x - dotPositions[2].x, dotPositions[1].y - dotPositions[2].y)
+//        let distance02 = hypot(dotPositions[0].x - dotPositions[2].x, dotPositions[0].y - dotPositions[2].y)
+//
+//        let cosRuleNumerator = pow(distance01, 2) +  pow(distance12, 2)  - pow(distance02, 2)
+//        let cosRuleDenominator = 2 * distance01 * distance12
+//        let cosAngle = cosRuleNumerator / cosRuleDenominator
+//        let angleRadians = acos(cosAngle)
+//        measuredAngle = 180 - (angleRadians * CGFloat(180 / CGFloat.pi));
+//    }
 }
